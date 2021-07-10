ea65d99fbf8004e537d000313e4aedf251b1c6c2
AnimeshGA <85989073+AnimeshGA@users.noreply.github.com>
2021-07-10 23:01:13 +0530

    Add files via upload

---
+++ area of cirlce.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 10 22:55:35 2021
+
+@author: Royal
+"""
+
+
+from math import pi
+r = float(input("Input the radius of the circle : "))
+print("The area of the circle with radius"+ str(r)+"is:"+ str(pi*r**2))
